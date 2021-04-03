d1a00e9d881ed187b7a175b81b0c5d82cd3c8524
elfungo <papafingoss@gmail.com>
2021-04-03 04:01:44 +0300

    fix api

--- api.js
+++ api.js
@@ -9,41 +9,45 @@ class API {
         this.state = {};
     }
 
-    start = async () => {
-        let binanceAPI    = new ccxt.binance ()
+    start() {
+        const self = this;
+        async function _start() {
+            let binanceAPI    = new ccxt.binance ()
 
 
-        const exchangeId = 'binance'
-            , exchangeClass = ccxt[exchangeId]
-            , exchange = new exchangeClass ({
-                'apiKey': 'YOUR_API_KEY',
-                'secret': 'YOUR_SECRET',
-                'timeout': 30000,
-                'enableRateLimit': true,
-            })
+            const exchangeId = 'binance'
+                , exchangeClass = ccxt[exchangeId]
+                , exchange = new exchangeClass ({
+                    'apiKey': 'YOUR_API_KEY',
+                    'secret': 'YOUR_SECRET',
+                    'timeout': 30000,
+                    'enableRateLimit': true,
+                })
 
-        await binanceAPI.loadMarkets()
+            await binanceAPI.loadMarkets()
 
-        let btc = new Combo("ETH/BTC", "BTC/USDT", "ETH/USDT", binanceAPI, 20000, 10);
+            let btc = new Combo("ETH/BTC", "BTC/USDT", "ETH/USDT", binanceAPI, 20000, 10);
 
 
-        let wallet = new Portfolio("ETH/BTC");
-        wallet.load(50, 2000, 0);
-        wallet.load(100/59, 59000, 1);
-        let tempWallet = wallet.clone();
+            let wallet = new Portfolio("ETH/BTC");
+            wallet.load(50, 2000, 0);
+            wallet.load(100/59, 59000, 1);
+            let tempWallet = wallet.clone();
 
-        btc.onTick((tick) => {
-            // console.log(tick);
-            const prices = [tick['ETH/USDT'].last, tick['BTC/USDT'].last, tick['ETH/BTC'].last];
-            const data = wallet.ai(prices);
+            btc.onTick((tick) => {
+                // console.log(tick);
+                const prices = [tick['ETH/USDT'].last, tick['BTC/USDT'].last, tick['ETH/BTC'].last];
+                const data = wallet.ai(prices);
 
-            console.log("---- WALLET ----");
-            console.log(`ETH : ${wallet.amount[0]}`);
-            console.log(`BTC : ${wallet.amount[1]}`);
-            this.state = data;
-        });
-        btc.tick();
-        this.comboCrawler = btc;
+                console.log("---- WALLET ----");
+                console.log(`ETH : ${wallet.amount[0]}`);
+                console.log(`BTC : ${wallet.amount[1]}`);
+                self.state = data;
+            });
+            btc.tick();
+            self.comboCrawler = btc;
+        }
+        _start();
     }
 
     stop = () => {
